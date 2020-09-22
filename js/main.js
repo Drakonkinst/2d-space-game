0bcd7fefb003cedfc81634ee3abc28631ae02e82
Drakonkinst <drakonkinstmc@gmail.com>
2020-09-22 15:46:18 -0700

    First universe

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 let canvas;
-let currentWorld;
+let universe;
 
 function setup() {
-    createCanvas(1000, 800);
+    canvas = createCanvas(1000, 800);
+    universe = new Universe();
+    universe.add(new Planetoid(100000, Vector.of(width / 2, height / 2), Vector.of(0, 0), 50, "blue"));
+    universe.add(new Planetoid(2, Vector.of(width / 2, height / 2 - 100), Vector.of(3, 0), 15, "red"));
+    
     Input.setup();
     console.log("Setup complete!");
 }
 
 function draw() {
+    universe.update(1);
     Graphics.draw();
 }
 
