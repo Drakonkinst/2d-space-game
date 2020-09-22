4199a54f3aad27ed8b27bf6cf419f763240e4bec
Drakonkinst <drakonkinstmc@gmail.com>
2020-09-22 15:45:02 -0700

    Added key mapping system

--- js/input.js
+++ js/input.js
@@ -2,10 +2,37 @@ function mouseClicked() {
     Input.mouseClicked();
 }
 
+function keyTyped() {
+    Input.onKey(key);
+}
+
 const Input = (function() {
+    let keyMap = {};
+    
     return {
         setup() {
-            
+            this.addOnKey("p", function() {
+                Config.drawAcceleration = !Config.drawAcceleration;
+                Config.drawVelocity = !Config.drawVelocity;
+            });
+        },
+        
+        addOnKey(key, callback) {
+            key = key.toLowerCase();
+            if(!keyMap.hasOwnProperty(key)) {
+                keyMap[key] = [];
+            }
+            let callbacks = keyMap[key];
+            callbacks.push(callback);
+        },
+        
+        onKey(key) {
+            if(keyMap.hasOwnProperty(key)) {
+                let callbacks = keyMap[key];
+                for(let callback of callbacks) {
+                    callback();
+                }
+            }
         },
         
         mouseClicked() {
