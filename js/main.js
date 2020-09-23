91c78ee2be25996062cb2cd0e3f262d13320d030
Drakonkinst <drakonkinstmc@gmail.com>
2020-09-23 15:48:19 -0700

    Added more scenarios

--- js/main.js
+++ js/main.js
@@ -16,12 +16,11 @@ const Scenario = (function() {
     };
 })();
 
-let TwoBodyScenario = new Scenario(
+let TwoBodyScenarioA = new Scenario(
     function() {
-        let center = Vector.of(width / 2, height / 2);
         universe = new Universe();
-        universe.add(new Planetoid("Blue", 15.0, center.copy(), Vector.of(0.5, 0), 50, "blue"));
-        universe.add(new Planetoid("Red", 1.0, center.copy().add(Vector.of(0, -110)), Vector.of(20, 0), 15, "red"));
+        universe.add(new Planetoid("Blue", 15.0, Vector.of(0, 0), Vector.of(0.5, 0), 50, "blue"));
+        universe.add(new Planetoid("Red", 1.0, Vector.of(0, -110), Vector.of(20, 0), 15, "red"));
         cameraFollow(universe.allBodies[0]);
     },
     
@@ -30,12 +29,25 @@ let TwoBodyScenario = new Scenario(
     }
 );
 
-let ThreeBodyScenario = new Scenario(
+let TwoBodyScenarioB = new Scenario(
     function() {
         universe = new Universe();
-        universe.add(new Planetoid("Blue", 15.0, center.copy(), Vector.of(0.5, 0), 50, "blue"));
-        universe.add(new Planetoid("Red", 1.0, center.copy().add(Vector.of(0, -150)), Vector.of(-15, 0), 15, "red"));
-        universe.add(new Planetoid("Gray", 1.0, center.copy().add(Vector.of(0, 200)), Vector.of(15, 0), 20, "gray"));
+        universe.add(new Planetoid("Blue", 1.0, Vector.of(0, 0), Vector.of(0.5, 0), 30, "blue"));
+        universe.add(new Planetoid("Red", 1.0, Vector.of(0, -110), Vector.of(5, 0), 30, "red"));
+        cameraFollow(universe.allBodies[0]);
+    },
+    
+    function() {
+        
+    }
+);
+
+let ThreeBodyScenarioA = new Scenario(
+    function() {
+        universe = new Universe();
+        universe.add(new Planetoid("Blue", 15.0, Vector.of(0, 0), Vector.of(0.5, 0), 50, "blue"));
+        universe.add(new Planetoid("Red", 1.0, Vector.of(0, -150), Vector.of(-15, 0), 15, "red"));
+        universe.add(new Planetoid("Gray", 1.0, Vector.of(0, 200), Vector.of(15, 0), 20, "gray"));
         cameraFollow(universe.allBodies[0]);
     },
 
@@ -44,32 +56,72 @@ let ThreeBodyScenario = new Scenario(
     }
 );
 
-/*
+let ThreeBodyScenarioB = new Scenario(
+    function() {
+        universe = new Universe();
+        universe.add(new Planetoid("Blue", 5.0, Vector.of(0, 0), Vector.of(0.5, 0), 30, "blue"));
+        universe.add(new Planetoid("Red", 1.0, Vector.of(0, -70), Vector.of(-8, 0), 15, "red"));
+        universe.add(new Planetoid("Gray", 1.0, Vector.of(0, 120), Vector.of(8, 0), 20, "gray"));
+        cameraFollow(universe.allBodies[0]);
+    },
+
+    function() {
+        
+    }
+);
+
+
+let ThreeBodyScenarioC = new Scenario(
+    function() {
+        universe = new Universe();
+        universe.add(new Planetoid("Blue", 1.0, Vector.of(0, 0), Vector.of(2, 2), 30, "blue"));
+        universe.add(new Planetoid("Red", 1.0, Vector.of(-100, -100), Vector.of(5, 0), 30, "red"));
+        universe.add(new Planetoid("Gray", 1.0, Vector.of(100, 100), Vector.of(5, 0), 30, "gray"));
+        cameraFollow(universe.allBodies[0]);
+    },
+
+    function() {
+        
+    }
+);
+
 let ThreeBodySolution = new Scenario(
     function() {
-        let center = Vector.of(width / 2, height / 2);
         universe = new Universe();
+        universe.G = 1;
         let posScale = 1;
-        let radius = 20;
-        let mass = 1;
-        universe.add(new Planetoid(mass, center.copy().add(Vector.of(-0.97000436, 0.24308753).scale(posScale)), Vector.of(0.4662036850, 0.4323657300), radius, "blue"));
-        universe.add(new Planetoid(mass, center.copy().add(Vector.of(0.97000436, -0.24308753).scale(posScale)), Vector.of(0.4662036850, 0.4323657300), radius, "red"));
-        universe.add(new Planetoid(mass, center.copy().add(Vector.of(0, 0)), Vector.of(-0.93240737, -0.86473146), radius, "gray"));
-        //cameraFollow(universe.allBodies[2]);
+        let radius = 1;
+        let surfaceGravity = 1;
+
+        let Blue = new Planetoid("Blue", surfaceGravity, Vector.of(-0.97000436, 0.24308753).scale(posScale), Vector.of(0.4662036850, 0.4323657300), radius, "blue");
+        let Red = new Planetoid("Red", surfaceGravity, Vector.of(0.97000436, -0.24308753).scale(posScale), Vector.of(0.4662036850, 0.4323657300), radius, "red");
+        let Gray = new Planetoid("Gray", surfaceGravity, Vector.of(0, 0), Vector.of(-0.93240737, -0.86473146), radius, "gray");
+
+        let displayRadius = .1;
+        Blue.radius = displayRadius;
+        Red.radius = displayRadius;
+        Gray.radius = displayRadius;
+
+        universe.add(Blue);
+        universe.add(Red);
+        universe.add(Gray);
+        cameraFollow(center);
+        Graphics.setZoom(200);
+        Config.updatesPerTick = 1;
+        Config.timestep = 0.02;
     },
 
     function () {
         
     }
-);*/
+);
 
 let SolarSystem = new Scenario(
     function() {
-        let center = Vector.of(width / 2, height / 2);
         universe = new Universe();
-        universe.add(new Planetoid("Sun", 28, center.copy(), Vector.of(1, 0), 70, "#FDB813"));
-        universe.add(new Planetoid("Earth", 1, center.copy().add(Vector.of(120, 0)), Vector.of(0, 35), 15, "green"));
-        universe.add(new Planetoid("Mars", 1, center.copy().add(Vector.of(180, 0)), Vector.of(0, 30), 20, "red"));
+        universe.add(new Planetoid("Sun", 28, Vector.of(0, 0), Vector.of(1, 0), 70, "#FDB813"));
+        universe.add(new Planetoid("Earth", 1, Vector.of(120, 0), Vector.of(0, 35), 15, "green"));
+        universe.add(new Planetoid("Mars", 1, Vector.of(180, 0), Vector.of(0, 30), 20, "red"));
         cameraFollow(universe.allBodies[0]);
     },
     
@@ -79,7 +131,7 @@ let SolarSystem = new Scenario(
 );
 
 function setup() {
-    setScenario(TwoBodyScenario);
+    setScenario(TwoBodyScenarioA);
     cameraFollow(universe.allBodies[0]);
     canvas = createCanvas();
     resetCanvas();
