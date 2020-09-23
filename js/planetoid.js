5992a8f383a55f0c3a49c9af4ba73e2530adfdef
Drakonkinst <drakonkinstmc@gmail.com>
2020-09-23 15:44:15 -0700

    Added Planetoid#pointOnPlanetoid

--- js/planetoid.js
+++ js/planetoid.js
@@ -1,9 +1,13 @@
 const Planetoid = (function() {
     return class Planetoid extends CelestialBody {
         constructor(name, surfaceGravity, position, initialVelocity, radius, color) {
-            super(name, surfaceGravity * radius * radius / Universe.getGravitationalConstant(), position, initialVelocity);
+            super(name, surfaceGravity * radius * radius / universe.getGravitationalConstant(), position, initialVelocity);
             this.radius = radius;
             this.color = color;
+        }
+        
+        pointOnPlanetoid(vector) {
+            return vector.distanceSquared(this.position) < this.radius * this.radius;
         }
     };
 })();
