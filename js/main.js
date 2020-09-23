e791a5d11779b39a53f1636d24df10d75beb56b9
Drakonkinst <drakonkinstmc@gmail.com>
2020-09-22 18:28:40 -0700

    Added cameraTargeting

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 let canvas;
 let universe;
+let cameraTarget;
 
 function setup() {
     canvas = createCanvas(1000, 800);
+    cameraTarget = Vector.of(0, 0);
     universe = new Universe();
     universe.add(new Planetoid(100000, Vector.of(width / 2, height / 2), Vector.of(0.1, 0), 50, "blue"));
     universe.add(new Planetoid(100, Vector.of(width / 2, height / 2 - 100), Vector.of(3, 0), 15, "red"));
@@ -11,7 +13,12 @@ function setup() {
     console.log("Setup complete!");
 }
 
+function cameraFollow(vector) {
+    cameraTarget = Vector.of(-vector.x + width / 2, -vector.y + height / 2);
+}
+
 function draw() {
+    cameraFollow(universe.allBodies[0].position);
     universe.update(1);
     Graphics.draw();
 }
